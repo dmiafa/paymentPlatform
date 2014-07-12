6b36e4160c13dd62860e7aa47b99cb81618ff83c
Dmitriy A <dmitriy.a@gmail.com>
2014-07-12 00:50:39 -0700

    stripe working, name form not connected to anything

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,24 +1,44 @@
 $(document).ready(function() {
-    var ticketPriceArray = $('.ticketPrice');
-    var ticketQuantityArray = $('.ticketQuantity');
-  var updatePrice = function() {
-          var totalPrice = 0;
-    for (var i = 0; i < ticketPriceArray.length; i++) {
-      try {
+  var ticketPriceArray = $('.ticketPrice');
+  var ticketQuantityArray = $('.ticketQuantity');
+
+  var calculatePriceInDollars = function() {
+    var totalPrice = 0;
+    try {
+      for (var i = 0; i < ticketPriceArray.length; i++) {
         totalPrice += parseInt(ticketPriceArray[i].innerHTML) * parseInt(
           ticketQuantityArray[i].value);
-      } catch (err) {
-        totalPrice = -1;
       }
+    } catch (err) {
+      alert("Sorry, something went wrong. Please refresh and try again.");
     }
-    $('#totalPrice')[0].innerHTML = totalPrice;
+    return totalPrice;
+  }
+  var calculateQuantity = function() {
+    var totalQuantity = 0;
+    try {
+      for (var i = 0; i < ticketQuantityArray.length; i++) {
+        totalQuantity += parseInt(ticketQuantityArray[i].value)
+      }
+    } catch (err) {
+      alert("Sorry, something went wrong. Please refresh and try again.");
+    }
+
+    return totalQuantity;
+
+  }
+
+  var updatePrice = function() {
+    var totalPriceInDollars = calculatePriceInDollars();
+    var totalQuantity = calculateQuantity();
+    $('#totalPrice')[0].innerHTML = totalPriceInDollars;
   }
 
   var validateQuantity = function() {
     for (var i = 0; i < ticketQuantityArray.length; i++) {
       try {
         var quantity = parseInt(ticketQuantityArray[i].value);
-        if(quantity<0){
+        if (quantity < 0) {
           ticketQuantityArray[i].value = 0;
           updatePrice();
           throw "Please select a positive number of tickets.";
@@ -30,10 +50,32 @@ $(document).ready(function() {
 
   }
 
+  $('#stripeCheckout').click(function() {
+    var token = function(res) {
+      var $input = $('<input type=hidden name=stripeToken />').val(res.id);
+      $('form').append($input).submit();
+    };
+    var totalPriceInDollars = calculatePriceInDollars();
+    var totalQuantity = calculateQuantity();
+    StripeCheckout.open({
+      key: 'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+      amount: totalPriceInDollars*100,
+      currency: 'usd',
+      name: 'NYC Loft Parties',
+      description: totalQuantity + " total ticket" + ((totalQuantity == 1) ?
+        "" : "s"),
+      panelLabel: 'Checkout',
+      token: token
+    });
+
+    return false;
+  })
+
   $('.ticketQuantity').change(function() {
-    updatePrice();
     validateQuantity();
+    updatePrice();
   });
+
   updatePrice();
 
 
